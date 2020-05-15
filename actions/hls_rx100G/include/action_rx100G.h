89423314d757ac3eac6b622bb5efabc3ad4e2379
fleon-psi <filip-karol.leonarski@psi.ch>
2020-05-15 22:04:01 +0200

    Typo fixed

--- actions/hls_rx100G/include/action_rx100G.h
+++ actions/hls_rx100G/include/action_rx100G.h
@@ -43,7 +43,7 @@
 #define MODE_PEDEG2         4
 #define MODE_RESET          5
 #define MODE_CONV_BSHUF     6
-#defone MODE_QUIT         255
+#define MODE_QUIT         255
 
 #define DELAY_FRAMES_STOP_AND_QUIT 5
 
